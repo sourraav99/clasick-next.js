14d81613761ba0fa4a2f3227a904b53dd2e43b5b
sourabh singh <sourabh@sourabhs-MacBook-Pro.local>
2024-12-31 01:01:23 +0530

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.hello next.js</li>
+          <li>Save and see your changes instantly.this is sourabh's next.js project</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
